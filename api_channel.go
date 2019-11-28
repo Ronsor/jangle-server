f2aa0bc7689c089c9aa544a8d2b637a511c692fb
Ronsor <ronsor@ronsor.pw>
2019-11-28 13:18:17 -0500

    big changes; DMs actually work!

---
+++ api_channel.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"time"
+
+	"jangled/util"
+
+	"github.com/valyala/fasthttp"
+	"github.com/fasthttp/router"
+	"github.com/bwmarrin/snowflake"
+)
+
+func InitRestChannel(r *router.Router) {
+	log.Println("Init /channels Endpoints")
+
+	type APIReqPostChannelsCidMessages struct {
+		Content string `json:"content"`
+		Nonce interface{} `json:"nonce"`
+		TTS bool `json:"tts"`
+		Embed interface{} `json:"embed"`
+		PayloadJson string `json:"payload_json"`
+	}
+
+	// Why is this so convoluted Discord? multipart/form-data, application/json, "payload_json"????
+	r.POST("/api/v6/channels/:cid/messages", MiddleTokenAuth(func(c *fasthttp.RequestCtx) {
+		me := c.UserValue("m:user").(*User)
+		var req APIReqPostChannelsCidMessages
+		cid := c.UserValue("cid").(string)
+		ct := string(c.Request.Header.Peek("Content-Type"))
+		if ct == "multipart/form-data" {
+			// TODO: something
+			// This is gonna be horrible to implement
+			panic("TODO")
+		} else {
+			err := util.ReadPostJSON(c, &req)
+			if err != nil {
+				util.WriteJSONStatus(c, 400, &APIResponseError{0, "Malformed request body"})
+				return
+			}
+		}
+
+		snow, err := snowflake.ParseString(cid)
+		if err != nil {
+			util.WriteJSONStatus(c, 404, &APIResponseError{10003, "Channel does not exist"})
+			return
+		}
+		ch, err := GetChannelByID(snow)
+		if err != nil {
+			util.WriteJSONStatus(c, 404, &APIResponseError{10003, "Channel does not exist"})
+			return
+		}
+
+		// Finally construct a minimal message object
+		// TODO: the rest lol
+		m := &Message{
+			Content: req.Content,
+			TTS: req.TTS,
+			Nonce: fmt.Sprintf("%v", req.TTS),
+			Author: &User{ID: me.ID},
+			Timestamp: time.Now().Unix(),
+		}
+
+		ch.CreateMessage(m)
+
+		return
+	}))
+}
