b57f7ec2b01aa0b868d4685cff9c060d0a1e1949
Ronsor <ronsor@ronsor.pw>
2020-01-24 10:59:09 -0500

    Allow unmarshalling multipart requests

--- util/req.go
+++ util/req.go
@@ -3,9 +3,40 @@ package util
 import (
 	"encoding/json"
 
+	"github.com/mitchellh/mapstructure"
 	"gopkg.in/go-playground/validator.v9"
 	"github.com/valyala/fasthttp"
 )
+
+func ReadPostAny(c *fasthttp.RequestCtx, i interface{}, opts ...interface{}) error {
+	frm, err := c.Request.MultipartForm()
+	if err != nil { return ReadPostJSON(c, i, opts...) }
+	var m map[string]interface{}
+	for k, v := range frm.Value {
+		m[k] = v[0]
+	}
+	for k, v := range frm.File {
+		m[k] = v[0]
+	}
+	if frm.Value["payload_json"] != nil {
+		err := json.Unmarshal([]byte(frm.Value["payload_json"][0]), i)
+		if err != nil { return err }
+	}
+	dec, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{TagName: "json", Result: i, WeaklyTypedInput: true})
+	if err != nil {
+		return err
+	}
+	err = dec.Decode(m)
+	if err != nil { return err }
+	if len(opts) == 0 || !opts[0].(bool) {
+		v := validator.New()
+		err = v.Struct(i)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func ReadPostJSON(c *fasthttp.RequestCtx, i interface{}, opts ...interface{}) error {
 	err := json.Unmarshal(c.PostBody(), i)
@@ -16,7 +47,6 @@ func ReadPostJSON(c *fasthttp.RequestCtx, i interface{}, opts ...interface{}) er
 		v := validator.New()
 		err = v.Struct(i)
 		if err != nil {
-			panic(err)
 			return err
 		}
 	}
